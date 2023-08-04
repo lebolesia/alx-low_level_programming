f700c3a56725fdc174f795dbd526c87b5948a3ca
lebolesia <lebohanglesia1999@gmail.com>
2023-08-04 14:08:03 -0700

    0-binary_to_uint.c#

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0;
+	unsigned int decimal_value = 0;
 
 	if (!b)
 		return (0);
@@ -18,8 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		decimal_value = 2 * decimal_value + (b[i] - '0');
 	}
 
-	return (dec_val);
+	return(deciimal_value);
 }
