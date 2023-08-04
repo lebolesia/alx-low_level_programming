55bc99dfed30092371c580118e49ac3714535504
lebolesia <lebohanglesia1999@gmail.com>
2023-08-04 14:29:43 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * @n: pointer to the number to change
  * @index: index of the bit to set to 1
  *
- * Return: 1if it worked, or -1 if an error occured
+ * Return: 1 if it worked, or -1 if an error occured
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
