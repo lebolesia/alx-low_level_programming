4299497f9bd785e4bfce60b219000be2ccc83c6a
lebolesia <lebohanglesia1999@gmail.com>
2023-08-04 14:12:22 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int decimal_value = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -18,8 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		decimal_value = 2 * decimal_value + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return(deciimal_value);
+	return(dec_val);
 }
