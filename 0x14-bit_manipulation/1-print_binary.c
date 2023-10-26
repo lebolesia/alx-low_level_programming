e0bb01fe1329b37b7dd72f8da355a7864a72f4bc
lebolesia <lebohanglesia1999@gmail.com>
2023-10-26 10:52:24 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _pow - The funct adds up (base ^ power)
+ * @base: The base of index
+ * @power: The capacity of index
+ * Return: The amount of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - It prints numb in binary symbols
+ * @n: The numb to print
+ * Return: invalid
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
+
