f068886246d0933ad828da22086fdd7f17f41ef7
lebolesia <lebohanglesia1999@gmail.com>
2023-12-24 10:21:00 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,28 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - forms hash table
- * @size: size of hash table
- * Return: pointer to hash table
+ * hash_table_create - Forms hash table
+ * @size: Size of array
+ * Return: On error - NULL
+ *         Or else - pointer to new hash table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *ht;
+	unsigned long int i;
 
-	if (size == 0)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	table = calloc(1, sizeof(hash_table_t));
-	if (table == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
-	{
-		free(table);
-		return (NULL);
-	}
-	return (table);
+	return (ht);
 }
